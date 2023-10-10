d07fc9315ace965ee286124177415a2534d167de
g56167 <56167@etu.he2b.be>
2023-10-10 18:05:04 +0200

    ajout fonction clone

--- seance4_cas1_js/car.js
+++ seance4_cas1_js/car.js
@@ -3,6 +3,10 @@ function Voiture(couleur) {
     this.couleur = couleur
 }
 
+Voiture.prototype.clone = function (voiture) {
+    Object.assign(this, voiture)
+}
+
 Voiture.prototype.packStandard = function () {
     this.radio = true
     this.telephone = true
@@ -21,6 +25,9 @@ Voiture.prototype.packSport = function () {
 let voitureStandard = new Voiture("bleu")
 voitureStandard.packStandard()
 
+let voitureStandard2 = new Voiture()
+voitureStandard2.clone(voitureStandard)
+
 let voitureFamiliale = new Voiture("noire")
 voitureFamiliale.packFamilial()
 
@@ -28,5 +35,6 @@ let voitureSportive = new Voiture("jaune")
 voitureSportive.packSport()
 
 console.log("Voiture standard : ", voitureStandard);
+console.log("Voiture standard n°2 : ", voitureStandard2);
 console.log("Voiture familiale : ", voitureFamiliale);
 console.log("Voiture sportive : ", voitureSportive);
